d16cf4a82a608d1a8937ce92dfa7c5449b6b44ff
xemlock <xemlock@gmail.com>
2016-07-12 21:32:40 +0100

    Fix loading images

--- configs/routes.config.php
+++ configs/routes.config.php
@@ -1,11 +1,13 @@
 <?php return array(
 
     'gallery.images' => array(
-        'route' => 'gallery/gallery-images/:dir_id',
+        'route' => 'gallery/gallery-images/:dir_id/:offset/:limit',
         'defaults' => array(
             'module'     => 'maniple-gallery',
             'controller' => 'gallery',
             'action'     => 'images',
+            'offset'     => null,
+            'limit'      => null,
         ),
     ),
 
